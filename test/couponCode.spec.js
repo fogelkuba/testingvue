cef8bd44a1eaa21ec9b85516ef28435263c00b36
fogelkuba <fogelkuba@gmail.com>
2019-05-03 21:06:31 +0200

    fix

--- test/couponCode.spec.js
+++ test/couponCode.spec.js
@@ -12,20 +12,23 @@ describe("CouponCode", () => {
     });
 
     it('validates a user-provided coupon code', () => {
-        let couponCode = wrapper.find('input.coupon-code');
-        couponCode.element.value = '50OFF';
-        couponCode.trigger('input');
+        enterCouponCode('50OFF');
+
         expect(wrapper.text()).toContain('Coupon Redeemed: 50% Off!')
     });
 
 
     it('broadcasts the percentage discount when a valid coupon code is applied', () => {
-            let couponCode = wrapper.find('input.coupon-code');
-            couponCode.element.value = '50OFF';
-            couponCode.trigger('input');
+            enterCouponCode('50OFF');
 
             expect(wrapper.emitted().aplied).toBeTruthy();
             expect(wrapper.emitted().aplied[0]).toEqual(50);
     });
+
+    function enterCouponCode(code) {
+        let couponCode = wrapper.find('input.coupon-code');
+        couponCode.element.value = '50OFF';
+        couponCode.trigger('input');
+    }
 
 }); 
