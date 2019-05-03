9b0d9ed6614ad3bbeb429b8b31a4e8e8dcce3a98
fogelkuba <fogelkuba@gmail.com>
2019-05-03 14:06:23 +0200

    fix

--- test/counter.spec.js
+++ test/counter.spec.js
@@ -1,12 +1,23 @@
 import {mount} from '@vue/test-utils';
-import Counter from './src/components/Counter.js';
+import Counter from '../src/components/Counter.js';
 import expect from 'expect';
 
 describe('Counter', () => {
+    let wrapper = mount(Counter);
 
-    if ('defaults to a count of 0', () => {
-        let wrapper = mount(Counter);
-        wrapper.vm.count
+    it ('defaults to a count of 0', () => {
+        expect(wrapper.vm.count).toBe(0)
     });
+
+    it('presents the current count', () => {
+        expect(wrapper.find('.count').html()).toContain(0)
+    });
+
+    it('increments the count when the button is clicked', () => {
+        expect(wrapper.vm.count).toBe(0)
+        wrapper.find('button').trigger('click');
+        expect(wrapper.vm.count).toBe(1)
+    });
+
 
 })
