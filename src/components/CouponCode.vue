0f37adac590d2a2b4a922ef0171a33c395de91d4
fogelkuba <fogelkuba@gmail.com>
2019-05-03 20:39:56 +0200

    fix

---
+++ src/components/CouponCode.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <input type="text" 
+               class="coupon-code" 
+               v-model="code" 
+               @input="validate"
+        />
+        <p v-if="valid">
+            Coupon Redeemend: {{message}}
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                code: '',
+                valid: false,
+                coupons: [
+                    {
+                        code: '50OFF',
+                        message: '50% Off!',
+                        discount: 50
+                    },
+                    {
+                        code: 'FREE',
+                        message: 'Entirely Free!',
+                        discount: 100
+                    },
+                ]
+            };
+        },
+        methods: {
+            validate() {
+                this.valid = this.coupons.map(coupon => coupon.code).includes(this.code);
+            }
+        },
+        computed: {
+            message() {
+                return this.coupons.find((coupon) => {
+                    return coupon.code === this.code;
+                }).message;
+            }
+        }
+    }
+</script>
