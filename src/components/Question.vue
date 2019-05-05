9d4c2ae1e946f95e42249fe66db9751b4dfedb9c
fogelkuba <fogelkuba@gmail.com>
2019-05-05 14:53:38 +0200

    fix

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,23 +1,34 @@
 <template>
-    <div>
+<div>
+    <!-- view the question -->
+    <div v-if="!editing">
         <h1>{{question.title}}</h1>
         <div class="body">{{question.body}}</div>
+        <button id="edit" v-if="editing" @click="editing = !editing">Edit</button>
     </div>
+
+    <!-- edit the question -->
+    <div v-else>
+        <input type="text" name="title" v-model="question.title">
+        <textarea type="text" name="body" v-model="question.body"></textarea>
+    </div>
+
+</div>
 </template>
 
 <script>
-    export default {
-        props: ['question'],
-        data () {
-            return {
-      
-            };
-        },
-        computed: {
-           
-        },
-        methods: {
-          
-        }
+export default {
+    props: ['question'],
+    data() {
+        return {
+            editing: false
+        };
+    },
+    computed: {
+
+    },
+    methods: {
+
     }
+}
 </script>
--- test/question.spec.js
+++ test/question.spec.js
@@ -11,7 +11,7 @@ describe('Question', () => {
         wrapper = mount(Question, {
             propsData: {
                 question: {
-                    title: 'The Title',
+                    title: 'The title',
                     body: 'The body'
                 }
             }
@@ -21,8 +21,20 @@ describe('Question', () => {
     it('presents the title and the body', () => {
         // expect(wrapper.html()).toContain('The Title');
         // expect(wrapper.html()).toContain('The body');
-        see('The Title');
+        see('The title');
         see('The body');
+    });
+
+    it('can be edited', () => {
+        expect(wrapper.contains('input[name=title]')).toBe(false)
+        wrapper.find('#edit').trigger('click');
+        expect(wrapper.find('input[name=title]').element.value).toBe('The title');
+        expect(wrapper.find('input[name=body]').element.value).toBe('The body');
+    });
+
+    it('hides the edit button during edit mode', () => {
+        wrapper.find('#edit').trigger('click');
+        expect(wrapper.contains('#edit')).toBe(false);
     });
 
     let see =  (text, selector) => {
