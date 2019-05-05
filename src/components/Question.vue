a1858bdf94255f66f3aa9bc12a1fa3bc58740a39
fogelkuba <fogelkuba@gmail.com>
2019-05-05 14:32:19 +0200

    fix

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,11 +1,13 @@
 <template>
     <div>
-
+        <h1>{{question.title}}</h1>
+        <div class="body">{{question.body}}</div>
     </div>
 </template>
 
 <script>
     export default {
+        props: ['question'],
         data () {
             return {
       
--- test/question.spec.js
+++ test/question.spec.js
@@ -2,16 +2,31 @@ import {
     mount
 } from 'vue-test-utils';
 import expect from 'expect';
-import Question from '../src/components/CouponCode.vue';
+import Question from '../src/components/Question.vue';
 
 describe('Question', () => {
    
     let wrapper;
     beforeEach( () => {
-        wrapper = mount(Question);
+        wrapper = mount(Question, {
+            propsData: {
+                question: {
+                    title: 'The Title',
+                    body: 'The body'
+                }
+            }
+        });
     });
 
     it('presents the title and the body', () => {
-        expect(wrapper.html()).toContain('The Title');
+        // expect(wrapper.html()).toContain('The Title');
+        // expect(wrapper.html()).toContain('The body');
+        see('The Title');
+        see('The body');
     });
+
+    let see =  (text, selector) => {
+        let wrap = selector ? wrapper.find(selector) : wrapper;
+        expect(wrap.html()).toContain(text);
+    }
 });
