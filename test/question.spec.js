fcc8ed9b3b313adf68c479eefd20bd9ec2c799e6
fogelkuba <fogelkuba@gmail.com>
2019-05-05 14:24:58 +0200

    fix

--- test/question.spec.js
+++ test/question.spec.js
@@ -1,7 +1,17 @@
-import {mount} from 'vue-test-utils';
+import {
+    mount
+} from 'vue-test-utils';
 import expect from 'expect';
 import Question from '../src/components/CouponCode.vue';
 
 describe('Question', () => {
    
+    let wrapper;
+    beforeEach( () => {
+        wrapper = mount(Question);
+    });
+
+    it('presents the title and the body', () => {
+        
+    })
 });
