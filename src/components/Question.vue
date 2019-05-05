673e89f85a90dbc29e1ca743c38589b0700bbc0c
fogelkuba <fogelkuba@gmail.com>
2019-05-05 15:15:51 +0200

    fix

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,37 +1,46 @@
 <template>
-<div>
-    <!-- view the question -->
-    <div v-if="!editing">
-        <h1>{{question.title}}</h1>
-        <div class="body">{{question.body}}</div>
-        <button id="edit" v-if="!editing" @click="editing = !editing">Edit</button>
+    <div>
+        <!-- Viewing the question. -->
+        <div v-if="! editing">
+            <h1 v-text="question.title"></h1>
+            <div class="body" v-text="question.body"></div>
+
+            <button id="edit" @click="editing = true">Edit</button>
+        </div>
+
+        <!-- Editing the question. -->
+        <div v-if="editing">
+            <input type="text" name="title" v-model="form.title">
+            <textarea name="body" v-model="form.body"></textarea>
+
+            <button id="cancel" @click="cancel">Cancel</button>
+            <button id="update" @click="update">Update</button>
+        </div>
     </div>
-
-    <!-- edit the question -->
-    <div v-else>
-        <input type="text" name="title" v-model="question.title">
-        <textarea type="text" name="body" v-model="question.body"></textarea>
-
-        <button id="update" @click="editing = false">Update</button>
-    </div>
-
-</div>
 </template>
 
 <script>
-export default {
-    props: ['question'],
-    data() {
-        return {
-            question: this.dataQuestion,
-            editing: false
-        };
-    },
-    computed: {
-
-    },
-    methods: {
-
+    export default {
+        props: ['dataQuestion'],
+        data () {
+            return {
+                question: this.dataQuestion,
+                form: {
+                    title: this.dataQuestion.title,
+                    body: this.dataQuestion.body,
+                },
+                editing: false
+            };
+        },
+        methods: {
+            cancel () {
+                this.editing = false;
+            },
+            update () {
+                this.question.title = this.form.title;
+                this.question.body = this.form.body;
+                this.editing = false;
+            }
+        }
     }
-}
 </script>
